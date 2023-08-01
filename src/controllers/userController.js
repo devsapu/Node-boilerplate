60d15d3b2e39f4ff37873ef6248f3046144190ac
Sapumal Thepulangoda <sapumal.t90@gmail.com>
2023-08-01 22:42:27 +0530

    adding jwt token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,23 @@
+const jwt = require('jsonwebtoken');
 const userService = require('../services/userService');
 
 exports.signin = async (req, res) => {
-    console.log('herer')
   const { name, contactNumber, email, username, password } = req.body;
   
   try {
     const newUser = await userService.createUser(name, contactNumber, email, username, password);
-    res.json(newUser);
+    
+    const payload = {
+      id: newUser.id,
+      username: newUser.username,
+      email: newUser.email,
+    };
+
+    const secretKey = '1234'; // In production, store this securely, not in plain text
+    const options = { expiresIn: '2h' };
+    const token = jwt.sign(payload, secretKey, options);
+
+    res.json({ newUser, token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -17,8 +28,24 @@ exports.login = async (req, res) => {
   
   try {
     const user = await userService.loginUser(username, password);
-    res.json(user);
+    if (!user) {
+      res.status(401).json({ message: 'Invalid username or password' });
+      return;
+    }
+
+    const payload = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const secretKey = '1234'; // In production, store this securely, not in plain text
+    const options = { expiresIn: '2h' };
+    const token = jwt.sign(payload, secretKey, options);
+
+    res.json({ user, token });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
